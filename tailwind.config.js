aa017bfab7b0355f2b2a36502194f4242d66dead
Cristian Orellana <cristianootj@gmail.com>
2022-09-24 15:30:18 -0300

    Cambios iniciales, prueba de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    fontFamily:{
+      raleway: ['Raleway'],
+    },
+    extend: {
+      colors:{
+        danger:'#FA7070'
+      }
+    },
+  },
+  plugins: [],
+}
